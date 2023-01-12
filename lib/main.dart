fab832c5461ee214f3c218aa7cc82f2123997a9b
Kenil <kenilraiyani95@gmail.com>
2023-01-12 15:54:12 +0530

    add_note_to_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -53,42 +53,41 @@ class HomePage extends StatelessWidget {
                           docToEdit: snapshot.data!.docs[index],
                         ));
                       },
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.blue.shade100),
+                      child: Card(
+                        color: Colors.blue.shade100,
+                        // decoration: BoxDecoration(
+                        //     borderRadius: BorderRadius.circular(10),
+                        //     color: Colors.blue.shade100),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             Align(
                               alignment: Alignment.centerLeft,
                               child: Padding(
-                                padding: const EdgeInsets.all(8.0),
+                                padding: const EdgeInsets.only(top: 8, left: 8),
                                 child: Text(
                                   snapshot.data!.docs[index]["title"],
                                   overflow: TextOverflow.ellipsis,
                                   textAlign: TextAlign.start,
                                   style: TextStyle(
-                                      fontSize: 19,
-                                      fontWeight: FontWeight.bold),
+                                    fontSize: 19,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
                             ),
                             Expanded(
-                              child: Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 9,
-                                    ),
-                                    child: Text(
-                                      snapshot.data!.docs[index]["content"],
-                                      overflow: TextOverflow.ellipsis,
-                                      textAlign: TextAlign.start,
-                                      style: TextStyle(),
-                                    ),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 8, vertical: 8),
+                                  child: Text(
+                                    snapshot.data!.docs[index]["content"],
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(),
                                   ),
-                                ],
+                                ),
                               ),
                             ),
                           ],
