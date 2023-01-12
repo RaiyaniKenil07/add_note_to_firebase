f3963a2f6967188d781a0d476af9e69c2c71571a
Kenil <kenilraiyani95@gmail.com>
2023-01-12 10:13:53 +0530

    add_note_to_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,10 @@ class HomePage extends StatelessWidget {
                 return GridView.builder(
                   physics: BouncingScrollPhysics(),
                   itemCount: snapshot.hasData ? snapshot.data!.docs.length : 0,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10),
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5),
                   itemBuilder: (context, index) {
                     return GestureDetector(
                       onTap: () {
@@ -54,31 +54,29 @@ class HomePage extends StatelessWidget {
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.blue.shade100),
-                        child: SingleChildScrollView(
-                          physics: BouncingScrollPhysics(),
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  snapshot.data!.docs[index]["title"],
-                                  style: TextStyle(
-                                      fontSize: 19,
-                                      fontWeight: FontWeight.bold),
-                                ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                snapshot.data!.docs[index]["title"],
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 19, fontWeight: FontWeight.bold),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 9,
-                                ),
-                                child: Text(
-                                  snapshot.data!.docs[index]["content"],
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(),
-                                ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 9,
                               ),
-                            ],
-                          ),
+                              child: Text(
+                                snapshot.data!.docs[index]["content"],
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     );
